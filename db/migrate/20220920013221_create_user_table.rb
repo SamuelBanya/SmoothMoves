765bd11b38be01fc3bfd28f9052866ba41674717
Samuel Banya <sbanya@fastmail.com>
2022-09-19 20:59:22 -0500

    Adding migration for 'User' table

---
+++ db/migrate/20220920013221_create_user_table.rb
@@ -0,0 +1,9 @@
+class CreateUserTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t| 
+      t.string :name
+      t.string :pickup_location
+      t.string :dropoff_location
+    end
+  end
+end

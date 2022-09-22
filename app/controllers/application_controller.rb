7bb0061a7a71fd222ac2f638d4485158852ab2ea
Samuel Banya <sbanya@fastmail.com>
2022-09-21 21:52:26 -0500

    Adding "patch" method to "items" table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -55,6 +55,17 @@ class ApplicationController < Sinatra::Base
   end
 
   patch "/items/:id" do 
+    item = Item.find(params[:item_id])
+    item.update(
+      name: params[:name],
+      owner: params[:owner],
+      length: params[:length],
+      width: params[:width],
+      height: params[:height],
+      weight: params[:weight],
+      item_type: params[:item_type]
+    )
+    item.to_json()
   end
 
   delete "/items/:id" do 

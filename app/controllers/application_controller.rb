6f5397849d6ba193f5b65875f0056c81776d665d
Samuel Banya <sbanya@fastmail.com>
2022-09-21 21:50:48 -0500

    Adding "get" and "post" method to "items" table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,9 +37,21 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/items" do 
+    items = Item.all()
+    items.to_json()
   end
 
   post "/items" do 
+    item = Item.create(
+      name: params[:name],
+      owner: params[:owner],
+      length: params[:length],
+      width: params[:width],
+      height: params[:height],
+      weight: params[:weight],
+      item_type: params[:item_type]
+    )
+    item.to_json()
   end
 
   patch "/items/:id" do 

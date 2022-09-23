087c3581e0a3d3c2f646496a9f6ea842824feb6c
Samuel Banya <sbanya@fastmail.com>
2022-09-23 10:10:01 -0500

    Adding revisions to 'application_controller' for 'moves' table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,38 +2,32 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
-
-  get "/users" do 
+  get "/moves" do 
     users = User.all()
     users.to_json()
   end
 
-  post "/users" do 
-    user = User.create(
-      name: params[:name],
+  post "/moves" do 
+    move = Move.create(
       pickup_location: params[:pickup_location],
       dropoff_location: params[:dropoff_location]
     )
     user.to_json()
   end
 
-  patch "/users/:id" do 
-    user = User.find(params[:user_id])
-    user.update(
-      name: params[:name],
+  patch "/moves/:id" do 
+    move = Move.find(params[:user_id])
+    move.update(
       pickup_location: params[:pickup_location],
       dropoff_location: params[:dropoff_location]
     )
-    user.to_json()
+    move.to_json()
   end
 
-  delete "/users/:id" do 
-    user = User.find(params[:user_id])
-    user.destroy()
-    user.to_json()
+  delete "/moves/:id" do 
+    move = Move.find(params[:user_id])
+    move.destroy()
+    move.to_json()
   end
 
   get "/items" do 
